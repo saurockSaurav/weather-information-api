2a39726c9da8a5cfb5f8530fed61db915b06f611
Saurav Thp <saurav.thapa2013@gmail.com>
2019-06-01 23:31:31 -0400

    reverse string and test cases

--- weather-rest-api-service/src/main/java/com/nextcitizen/weather/app/utils/WeatherAppUtils.java
+++ weather-rest-api-service/src/main/java/com/nextcitizen/weather/app/utils/WeatherAppUtils.java
@@ -2,8 +2,19 @@ package com.nextcitizen.weather.app.utils;
 
 public class WeatherAppUtils {
 	
-	public static boolean isNumeric(String value) {
+	public static void main(String[] args) {
+		WeatherAppUtils weatherAppUtils = new WeatherAppUtils();
+		System.out.println(weatherAppUtils.isNumeric("9"));
+		System.out.println(weatherAppUtils.reverseTheString("RaceCar"));
+	}
+	
+	public boolean isNumeric(String value) {
 		return value.matches("[0-9]+");
+	}
+	
+	public String reverseTheString(final String str) {
+		StringBuilder strBulder= new StringBuilder(str);
+		return strBulder.reverse().toString();
 	}
 	
 }
---
+++ weather-rest-api-service/src/test/java/com/example/demo/WeatherAppUtilsTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nextcitizen.weather.app.utils.WeatherAppUtils;
+
+public class WeatherAppUtilsTest {
+	
+	@Autowired
+	private WeatherAppUtils	weatherAppUtils;
+
+	@Test
+	public void testreverseTheString1() {
+		assertEquals("racecar", weatherAppUtils.reverseTheString("racecar"));
+
+	}
+	
+	@Test
+	public void testreverseTheString2() {
+		assertNotEquals("father", weatherAppUtils.reverseTheString("father"));
+
+	}
+
+	@Test
+	public void testIsNumeric() {
+		assertTrue(weatherAppUtils.isNumeric("12"));
+		
+	}
+	
+	
+	
+	
+	
+}
